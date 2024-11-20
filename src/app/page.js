1c3a089f43b114f57ab5dc865e0d7b94ee0e27f7
Mateus <mateushleonardi@gmail.com>
2024-11-20 14:55:49 -0300

    HomePage add

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import "./css/home.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1>Hello World!!</h1>    
-    </div>
+    <nav>
+     <h1>Logo</h1>
+     <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="#">Contact</a></li>
+     </ul>
+    </nav>
   );
 }
